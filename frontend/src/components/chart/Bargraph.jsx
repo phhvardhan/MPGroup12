4e2e4dc0685f81adfa72d2bbbf0b4473b302b954
phhvardhan <harshavardhanpeela055@gmail.com>
2022-01-03 19:52:09 +0530

    Commit

--- frontend/src/components/chart/Bargraph.jsx
+++ frontend/src/components/chart/Bargraph.jsx
@@ -2,47 +2,51 @@ import React from 'react'
 import { CCard, CCardBody, CCol, CCardHeader, CRow } from '@coreui/react'
 import { CChartBar, CChartPie } from '@coreui/react-chartjs'
 
-const Charts = () => 
-{
-<CCol xs={2}>
-        <CCard className="mb-1">
-          <CCardHeader>Pie Chart</CCardHeader>
-          <CCardBody>
-            <CChartPie
-              data={{
-                labels: [
-                  '0 Backlogs',
-                  '1 Backlog',
-                  '2 Backlogs',
-                  '3 Backlogs',
-                  '5 Backlogs',
-                  '6 Backlogs',
+const Charts = () => {
+  return (
+    <CCol xs={2}>
+    <CCard className="mb-1">
+      <CCardHeader>Bar Chart</CCardHeader>
+      <CCardBody>
+        <CChartBar
+          data={{
+            labels: [
+              '0 Backlogs',
+              '1 Backlog',
+              '2 Backlogs',
+              '3 Backlogs',
+              '5 Backlogs',
+              '6 Backlogs',
+            ],
+            datasets: [
+              {
+                data: [210, 20, 30, 10, 24],
+                backgroundColor: [
+                  '#FF6384',
+                  '#36A2EB',
+                  '#FFCE56',
+                  '#41B883',
+                  '#E46651',
+                  '#00D8FF',
                 ],
-                datasets: [
-                  {
-                    data: [210, 20, 30, 10, 24],
-                    backgroundColor: [
-                      '#FF6384',
-                      '#36A2EB',
-                      '#FFCE56',
-                      '#41B883',
-                      '#E46651',
-                      '#00D8FF',
-                    ],
-                    hoverBackgroundColor: [
-                      '#FF6384',
-                      '#36A2EB',
-                      '#FFCE56',
-                      '#41B883',
-                      '#E46651',
-                      '#00D8FF',
-                    ],
-                  },
+                hoverBackgroundColor: [
+                  '#FF6384',
+                  '#36A2EB',
+                  '#FFCE56',
+                  '#41B883',
+                  '#E46651',
+                  '#00D8FF',
                 ],
-              }}
-            />
-          </CCardBody>
-        </CCard>
-      </CCol>
+                data: [210, 20, 30, 10, 24],
+              },
+            ],
+          }}
+          labels="months"
+        />
+      </CCardBody>
+    </CCard>
+  </CCol>
+  )
 }
+
 export default Charts
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -6,11 +6,13 @@ import {userData} from "../../dummyData";
 import WidgetSm from '../../components/widgetSm/WidgetSm';
 import WidgetLg from '../../components/widgetLg/WidgetLg';
 import PieChart from '../../components/chart/PieChart';
+import Bargraph from '../../components/chart/Bargraph'
 const Home = ({failCount,passCount, Registered}) => {
     return ( 
         <div className='home'>
             <FeaturedInfo  failCountData={failCount} passCountData={passCount} RegisteredData={Registered} />
             <PieChart/>
+            <Bargraph/>
             <Chart data={userData} title="User Analytics" grid datakey="Active User" />
             <div className="homeWidgets">
                 <WidgetSm />
